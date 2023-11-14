cc72e9d6dc7abce3d7faee75160d144b7c93f9a0
Utkarsh gupta <122604770+Shadowsweep@users.noreply.github.com>
2023-11-15 01:26:00 +0530

    Update qrscanner.py

--- qrgenv1/qrscanner.py
+++ qrgenv1/qrscanner.py
@@ -1,18 +1,19 @@
-import cv2
-import webbrowser
+#This file has been committed dur to errors in import This is the code to scan qr code 
+#import cv2
+# import webbrowser
 
-cap = cv2.VideoCapture(0)
-detector = cv2.QRCodeDetector()
+# cap = cv2.VideoCapture(0)
+# detector = cv2.QRCodeDetector()
 
-while True:
-    _,img = cap.read()
-    data,one, _=detector.detectAndDecode(img)
-    if data:
-        a = data
-        break
-    cv2.imshow('qr codescanner app',img)
-    if cv2.waitKey(1)==ord('q'):
-        break
-b = webbrowser.open(str(a))
-cap.release(a) 
-cv2.destroyAllWindows()
+# while True:
+#     _,img = cap.read()
+#     data,one, _=detector.detectAndDecode(img)
+#     if data:
+#         a = data
+#         break
+#     cv2.imshow('qr codescanner app',img)
+#     if cv2.waitKey(1)==ord('q'):
+#         break
+# b = webbrowser.open(str(a))
+# cap.release(a) 
+# cv2.destroyAllWindows()
